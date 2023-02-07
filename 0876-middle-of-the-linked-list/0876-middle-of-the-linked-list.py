de8bd2ed385ec062a4c9d32e1b4667beac56ffc2
mistersoh <mistersoh@naver.com>
2023-02-07 14:05:57 +0900

    Time: 24 ms (96.71%), Space: 13.9 MB (48.13%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -5,22 +5,8 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        def getCount(self, node: Optional[ListNode]):
-            if not node:
-                return 0
-            else:
-                return 1 + getCount(self, node.next)
-        full_len = getCount(self, head)
-
-        # print(full_len)
-        
-        middle_len = full_len // 2
-        
-        temp_m = head
-        while middle_len != 0:
-            temp_m = temp_m.next
-            # print(temp_m)
-            middle_len -= 1
-            
-        return temp_m
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow

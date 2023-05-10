9f24fda052f6c859783c0c61dab6b85b2f4f115c
서원우 <mistersoh@naver.com>
2023-05-10 16:15:25 +0900

    Time: 114 ms (33.31%), Space: 21.3 MB (8.53%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,8 +1,15 @@
-from collections import Counter
-
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        most_common_num = (Counter(nums).most_common(k))
-        
-        return [num[0] for num in most_common_num]
+import heapq
+from collections import Counter, defaultdict
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        heap = [(-1*v1, k1) for k1,v1 in Counter(nums).items()]
+        heapq.heapify(heap)
+        result = []
+        for i in range(k):
+            result.append(heapq.heappop(heap)[1])
+        return result

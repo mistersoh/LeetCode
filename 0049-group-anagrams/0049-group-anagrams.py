a14e4eb338f895d852df0c1f7dd34e503f7cd3b7
서원우 <mistersoh@naver.com>
2023-05-10 14:46:34 +0900

    Time: 120 ms (23.84%), Space: 19.8 MB (27.35%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,16 @@
-from collections import Counter
 from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        answer = []
         
         strs_hash = defaultdict(list)
         
         for s in (strs):
             
-            s_Counter = Counter(sorted(s))
+            s_sorted = str(sorted(s))
             
-            strs_hash[str(s_Counter)].append(s)
+            strs_hash[s_sorted].append(s)
         
         return (list(strs_hash.values()))
             

a668b9b8626486baf5a3e967f653425a209388e6
서원우 <mistersoh@naver.com>
2024-05-12 21:25:48 +0900

    Time: 692 ms (56.50%), Space: 76.9 MB (43.94%) - LeetHub

---
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
@@ -0,0 +1,37 @@
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+  
+         # First Attempt
+#         def quickSort(fractions):
+#             if (len(fractions) <= 1):
+#                 return fractions
+            
+#             pivot = fractions[len(fractions)//2]
+#             l,r,e = [],[],[]
+            
+#             for a in fractions:
+#                 if a[0]*pivot[1] < a[1]*pivot[0]:
+#                     l.append(a)
+#                 elif a[0]*pivot[1] > a[1]*pivot[0]:
+#                     r.append(a)
+#                 else:
+#                     e.append(a)
+        
+#             return quickSort(l) + e + quickSort(r)
+        
+        fractions = []
+        cnt = 0
+        
+        for idx, val in enumerate(arr):
+            subarr = arr[idx+1:][::-1]
+            for num in subarr:
+                cnt += 1
+                fractions += [(val, num)]
+                
+        sorted_fractions = sorted(fractions, key=lambda x: x[0]/x[1])
+        
+        return sorted_fractions[k-1]
+        
+        
+            
+            
